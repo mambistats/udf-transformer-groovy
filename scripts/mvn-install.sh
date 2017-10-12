838a350590007c53fca7cd59a2d78bae5ee307e5
frank.kemmer@1und1.de <frank.kemmer@1und1.de>
2017-10-12 12:21:44 +0200

    Convenience script for maven install

---
+++ scripts/mvn-install.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+mvn install:install-file \
+  -Dfile="${BASEDIR}/target/udf-transformer-groovy-0.0.1-SNAPSHOT-jar-with-dependencies.jar" \
+  -DgroupId="com.kc14.hadoop.hive" \
+  -DartifactId="udf-transformer-groovy" \
+  -Dversion="0.0.1" \
+  -Dpackaging="jar"
