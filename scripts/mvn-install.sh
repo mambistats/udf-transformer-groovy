ad0706ec228f6057b870b3b1b49379f4b6904157
frank.kemmer@1und1.de <frank.kemmer@1und1.de>
2017-10-13 11:57:27 +0200

    Sync Fork

--- scripts/mvn-install.sh
+++ scripts/mvn-install.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+source "${HOME}/.bashrc"
 mvn install:install-file \
   -Dfile="${BASEDIR}/target/udf-transformer-groovy-0.0.1-SNAPSHOT-jar-with-dependencies.jar" \
   -DgroupId="com.kc14.hadoop.hive" \
