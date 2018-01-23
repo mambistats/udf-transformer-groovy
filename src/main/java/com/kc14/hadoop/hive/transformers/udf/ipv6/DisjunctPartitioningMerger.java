7a5166ec3089a813dc3e1434a24b46a113195810
frank.kemmer@1und1.de <frank.kemmer@1und1.de>
2018-01-23 17:16:24 +0100

    Merge different IP Ranges into one IP Ranges Index

---
+++ src/main/java/com/kc14/hadoop/hive/transformers/udf/ipv6/DisjunctPartitioningMerger.java
@@ -0,0 +1,226 @@
+package com.kc14.hadoop.hive.transformers.udf.ipv6;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.kc14.janvanbesien.com.googlecode.ipv6.IPv6Address;
+import com.kc14.janvanbesien.com.googlecode.ipv6.IPv6AddressRange;
+
+public class DisjunctPartitioningMerger {
+	
+	// Using closed integer interval arithmetic!
+	public static <E> void merge(final Iterator<IPv6AddressRange> A, final Iterator<IPv6AddressRange> B,
+			                     final Iterator<Collection<E>> AP, final Iterator<Collection<E>> BP,
+			                     final Collection<IPv6AddressRange> C, final Collection<Collection<E>> CP,
+			                     Collection<E> prototype // Type Tagger
+			                     ) {
+		IPv6AddressRange a =  getNextRange(A);
+		Collection<E>    ap = getNextProperties(AP);
+		
+		IPv6AddressRange b =  getNextRange(B);
+		Collection<E>    bp = getNextProperties(BP);
+		
+		while (a != null && b != null) {
+			if (a.getLast().compareTo(b.getFirst()) < 0) {
+				// Non-overlapping case 1
+				// Interval A: |-----|
+				// Interval B:         |-----|
+				C.add(a);
+				CP.add(appendNull(ap, prototype));
+				a = getNextRange(A);
+				ap = getNextProperties(AP);
+			}
+			else if (b.getLast().compareTo(a.getFirst()) < 0) {
+				// Non-overlapping case 2
+				// Interval A:         |-----|
+				// Interval B: |-----|
+				C.add(b);
+				CP.add(prependNull(bp, prototype));
+				b = getNextRange(B);
+				bp = getNextProperties(BP);
+			}
+			else {
+				// Intersection between A and B
+				final int A_Left_CompareTo_B_Left = a.getFirst().compareTo(b.getFirst());
+				final int A_Right_CompareTo_B_Right = a.getLast().compareTo(b.getLast());
+				if (A_Left_CompareTo_B_Left < 0) {
+					// Interval A: |--+-----....
+					// Interval B:    |-------....
+					C.add(IPv6AddressRange.fromFirstAndLast(a.getFirst(), predecessor(b.getFirst())));
+					CP.add(appendNull(ap, prototype));
+					if (A_Right_CompareTo_B_Right < 0) {
+						// Interval A: |--+-----|
+						// Interval B:    |-----+-----|
+						C.add(IPv6AddressRange.fromFirstAndLast(b.getFirst(), a.getLast())); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						b = IPv6AddressRange.fromFirstAndLast(successor(a.getLast()), b.getLast());
+						// bp = bp;
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+					}
+					else if (A_Right_CompareTo_B_Right > 0) {
+						// Interval A: |--+------+-|
+						// Interval B:    |------|
+						C.add(b); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = IPv6AddressRange.fromFirstAndLast(successor(b.getLast()), a.getLast());
+						// ap = ap;
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+					else {
+						// Interval A: |--+--------|
+						// Interval B:    |--------|
+						C.add(b); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+				}
+				else if (A_Left_CompareTo_B_Left > 0) {
+					// Interval A:    |-------....
+					// Interval B: |--+-----....
+					C.add(IPv6AddressRange.fromFirstAndLast(b.getFirst(), predecessor(a.getFirst())));
+					CP.add(prependNull(bp, prototype));
+					if (A_Right_CompareTo_B_Right > 0) {
+						// Interval A:    |-----+-----|
+						// Interval B: |--+-----|
+						C.add(IPv6AddressRange.fromFirstAndLast(a.getFirst(), b.getLast())); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = IPv6AddressRange.fromFirstAndLast(successor(b.getLast()), a.getLast());
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+					else if (A_Right_CompareTo_B_Right < 0) {
+						// Interval A:    |------|
+						// Interval B: |--+------+-|
+						C.add(a); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						b = IPv6AddressRange.fromFirstAndLast(successor(a.getLast()), b.getLast());
+						// bp = bp;
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+					}
+					else {
+						// Interval A:    |--------|
+						// Interval B: |--+--------|
+						C.add(a); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+				}
+				else {
+					// Interval A: |-------....
+					// Interval B: |-------....
+					if (A_Right_CompareTo_B_Right < 0) {
+						// Interval A: |-----|
+						// Interval B: |-----+-----|
+						C.add(a); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						b = IPv6AddressRange.fromFirstAndLast(successor(a.getLast()), b.getLast());
+						// bp = bp;
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+					}
+					else if (A_Right_CompareTo_B_Right > 0) {
+						// Interval A: |-----+-----|
+						// Interval B: |-----|
+						C.add(b); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = IPv6AddressRange.fromFirstAndLast(successor(b.getLast()), a.getLast());
+						// bp = bp;
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+					else {
+						// Interval A: |--------|
+						// Interval B: |--------|
+						C.add(a); // A v B
+						CP.add(sum(ap, bp, prototype)); // New interval gets properties of both intervals
+						a = getNextRange(A);
+						ap = getNextProperties(AP);
+						b = getNextRange(B);
+						bp = getNextProperties(BP);
+					}
+				}
+			}			
+		}		
+		if (a != null) { // Boundary solution: B completely processed or empty, but right boundary of current interval in A not!
+			// Append interval in progress
+			C.add(a);
+			CP.add(appendNull(ap, prototype));
+			// Append rest of A
+			while (A.hasNext()) C.add(A.next());
+			while (AP.hasNext()) CP.add(appendNull(AP.next(), prototype));
+		}
+		else if (b != null) { // Boundary solution: A completely processed or empty, but right boundary of current interval in B not!
+			// Append interval in progress
+			C.add(b);
+			CP.add(prependNull(bp, prototype));
+			// Append rest of B
+			while (B.hasNext()) C.add(B.next());
+			while (BP.hasNext()) CP.add(prependNull(BP.next(), prototype));
+		}
+		return;				
+	}
+
+
+	private static IPv6AddressRange getNextRange(Iterator<IPv6AddressRange> A) {
+		return A.hasNext() ? A.next() : null;
+	}
+
+	private static <E> Collection<E> getNextProperties(Iterator<Collection<E>> P) {
+		return P.hasNext() ? P.next() : null;
+	}
+	
+	
+	private static <E> Collection<E> appendNull(Collection<E> ap, Collection<E> prototype) {
+		Collection<E> cp = cloneByType(prototype, ap.size() + 1);
+		cp.addAll(ap);
+		cp.add(null);
+		return cp;
+	}
+
+	private static <E> Collection<E> prependNull(Collection<E> ap, Collection<E> prototype) {
+		Collection<E> cp = cloneByType(prototype, ap.size() + 1);
+		cp.add(null);
+		cp.addAll(ap);
+		return cp;
+	}
+
+	private static <E> Collection<E> sum(Collection<E> ap, Collection<E> bp, Collection<E> prototype) {
+		Collection<E> cp = cloneByType(prototype, ap.size() + bp.size());
+		cp.addAll(ap);
+		cp.addAll(bp);
+		return cp;
+	}
+
+	private static <E> Collection<E> cloneByType (Collection<E> collection, int capacity) {
+		Collection<E> newInstance = null;
+		try {
+			newInstance = collection.getClass().getConstructor(Integer.TYPE).newInstance(capacity);
+		} catch (InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			// Choose a collection with a capacity constructor
+			e.printStackTrace();
+		}
+		return newInstance;
+	}
+
+	
+	private static IPv6Address successor(final IPv6Address ipaddr) {
+		return ipaddr.add(1); // ++addr
+	}
+
+	private static IPv6Address predecessor(final IPv6Address ipaddr) {
+		return ipaddr.subtract(1); // --addr
+	}
+	
+}
